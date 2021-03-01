4899ea3d9955d40eba271968dfd7910e25d1217e
Cahyo <cariercahyo@gmail.com>
2021-03-01 15:10:54 +0900

    add modal search

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -66,6 +66,16 @@
     @yield('content')
 
 		<br/>
+
+		<div id="searchModal" class="search-modal">
+			<button type="button" class="close" aria-label="Close" data-dismiss="searchModal">
+					<span aria-hidden="true">&times;</span>
+			</button>
+			<form action="index.html" method="get" class="oleez-overlay-search-form">
+					<label for="search" class="sr-only">Search</label>
+					<input type="search" class="oleez-overlay-search-input" id="search" name="search" placeholder="Search here" autocomplete="off">
+			</form>
+		</div>
     
     @section('footer')
 		<footer class="oleez-footer">

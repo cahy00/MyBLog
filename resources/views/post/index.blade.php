47189f554a4c4f4678777605bded488a9f4c236f
Cahyo <cariercahyo@gmail.com>
2021-03-02 13:22:37 +0900

    add show category

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -11,7 +11,7 @@
 							{{-- Format waktu January 29, 2020 --}}
 							<p class="post-date">{{$item->created_at->format('M d, Y')}} by Sulistyo Cahyo</p> 
 							<h4 class="post-title"><a href="/show-post/{{$item->id}}" style="text-decoration: none; color:black">{{$item->title}}</a></h4>
-							<p class="post-excerpt">{!!substr($item->body, 0, 150)!!}...</p>
+							<p class="post-excerpt">{!!substr($item->body, 0, 150)!!} <span>....</span></p>
 							<a href="/show-post/{{$item->id}}" class="post-permalink">READ MORE</a>
 						</article>
 					@endforeach

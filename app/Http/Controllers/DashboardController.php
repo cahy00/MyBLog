1169f3eeb7a76e0a9796b1e0d7cff46404fe6e2b
Cahyo <cariercahyo@gmail.com>
2021-03-09 00:00:49 +0900

    update input admin post

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,13 +18,34 @@ class DashboardController extends Controller
 
 		public function store(Request $request)
 		{
-			$request->validate([
-				'title' 	 => 'required|min:10|max:30',
-				'category' => 'required|max:15',
-				'tag[]' 	 => 'required|array',
-				'image' 	 => 'required|mimes:jpg,jpeg,png',
-				'body'		 => 'required'
-			]);
-			return $request->all();
+			try{
+				$request->validate([
+					'title' 	 => 'required|min:10|max:30',
+					'category' => 'required|max:15',
+					// 'tag[]' 	 => 'required',
+					'image' 	 => 'required|mimes:jpg,jpeg,png',
+					// 'body'		 => 'required'
+				]);
+
+				$file = $request->file('image');
+				$name = time();
+				$extFile = $file->getOriginalFileExtension();
+				$newName = $name . "." . $extFile;
+
+
+				$post = Post::create([
+					'title' => $request->title,
+					'body'  => $request->body,
+					'image' => 'storage/img/' . $newName,
+					'slug'	=> Str::slug($request->title),
+					
+					
+				]);
+				return $request->all();
+			}catch(\Exception $e){
+				return $e->getMessage();
+			}
+
+			
 		}
 }
